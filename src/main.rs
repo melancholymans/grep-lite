dee9478da0ffedd025d481b5d4b3b988791dc366
joshua <kjkq26993@gaia.eonet.ne.jp>
2022-09-23 10:45:50 +0900

    最終形

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,12 @@
-use regex::Regex;    // <1>
-use clap::{App,Arg};
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
 
 fn main() {
-  let args = App::new("grep-lite")
-    .version("0.1")
-    .about("searches for patterns")
-    .arg(Arg::with_name("pattern")
-      .help("The pattern to search for")
-      .takes_value(true)
-      .required(true))
-    .get_matches();
-  let pattern = args.value_of("pattern").unwrap();
-  let re = Regex::new(pattern).unwrap();    // <2>
-
-  let quote = "Every face, every shop, bedroom window, public-house, and
-dark square is a picture feverishly turned--in search of what?
-It is the same with books. What do we seek through millions of pages?";
-
-  for line in quote.lines() {
-    match re.find(line) {    // <3>
-        Some(_) => println!("{}", line),    // <4>
-        None => (),    // <5>
-    }
+  let f = File::open("READ.md").unwrap();    // <1>
+  let reader = BufReader::new(f);
+  for line_ in reader.lines(){
+    let line = line_.unwrap(); // <3>
+    println!("{} ({} bytes long)", line, line.len());
   }
 }
