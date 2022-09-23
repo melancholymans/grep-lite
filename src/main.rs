f2f20682df9aca504b96a2278e44dcccaec38199
joshua <kjkq26993@gaia.eonet.ne.jp>
2022-09-23 09:32:21 +0900

    引数機能の追加

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
 use regex::Regex;    // <1>
+use clap::{App,Arg};
 
 fn main() {
-  let re = Regex::new("picture").unwrap();    // <2>
+  let args = App::new("grep-lite")
+    .version("0.1")
+    .about("searches for patterns")
+    .arg(Arg::with_name("pattern")
+      .help("The pattern to search for")
+      .takes_value(true)
+      .required(true))
+    .get_matches();
+  let pattern = args.value_of("pattern").unwrap();
+  let re = Regex::new(pattern).unwrap();    // <2>
 
   let quote = "Every face, every shop, bedroom window, public-house, and
 dark square is a picture feverishly turned--in search of what?
 It is the same with books. What do we seek through millions of pages?";
 
   for line in quote.lines() {
-    let contains_substring = re.find(line);
-    match contains_substring {    // <3>
-
+    match re.find(line) {    // <3>
         Some(_) => println!("{}", line),    // <4>
         None => (),    // <5>
     }
